75817236bfd329852559af526635f71a173879f4
belousov <belousov.nikolai@gmail.com>
2020-05-15 00:24:45 +0300

    Added 'factory-method' implementation

---
+++ src/com/designpatterns/factory_method/Shape.java
@@ -0,0 +1,5 @@
+package com.designpatterns.factory_method;
+
+public interface Shape {
+    void draw();
+}
---
+++ src/com/designpatterns/factory_method/ShapeFactory.java
@@ -0,0 +1,5 @@
+package com.designpatterns.factory_method;
+
+public interface ShapeFactory {
+    Shape createShape(String shape);
+}
