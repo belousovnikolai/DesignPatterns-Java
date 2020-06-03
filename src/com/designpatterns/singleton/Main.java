eccf3de398d3bf65d8708fb83f9081ee8ca4ab28
belousov <belousov.nikolai@gmail.com>
2020-06-04 00:54:50 +0300

    Added 'singleton' pattern implementation

---
+++ src/com/designpatterns/singleton/Main.java
@@ -0,0 +1,12 @@
+package com.designpatterns.singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance("CHECK");
+        Singleton singleton1 = Singleton.getInstance("OUT");
+
+        // Printed values will be the same as only one Singleton was created
+        System.out.println(singleton.foo);
+        System.out.println(singleton1.foo);
+    }
+}
